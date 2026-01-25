07b27fd7adea78262b3d5a1af4ad9e7632ba8b75
Nilesh Kaute <nileshkaute1@gmail.com>
2026-01-25 12:09:36 +0530

    feat: integrate new sections into home page

--- music/src/pages/Home.jsx
+++ music/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Hero from "../component/home/Hero";
 import CreativeProductSection from "../component/home/CreativeProductSection";
 import ProductSection from "../component/home/ProductSection";
 import Footer from "../component/home/Footer";
+import TrustSection from "../component/home/TrustSection";
+import TestimonialsSection from "../component/home/TestimonialsSection";
+import NewsletterSection from "../component/home/NewsletterSection";
 
 const Home = () => {
   return (
@@ -10,11 +13,20 @@ const Home = () => {
       {/* Hero Section */}
       <Hero />
 
+      {/* Trust Signals Section */}
+      <TrustSection />
+
       {/* Products Section */}
       <ProductSection />
 
       {/* Creative Product Section */}
       <CreativeProductSection />
+
+      {/* Testimonials Section */}
+      <TestimonialsSection />
+
+      {/* Newsletter Section */}
+      <NewsletterSection />
 
       {/* Footer */}
       <Footer />
